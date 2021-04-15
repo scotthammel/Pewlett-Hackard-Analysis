a56d6f1c2961a417b92ac34dcbeb54658ba38701
Scott Hammel <scott_hammel@icloud.com>
2021-04-14 20:27:10 -0500

    added CREATE TABLE script.

---
+++ schema.sql
@@ -0,0 +1,63 @@
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+-- Create another table for Employees
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+-- Create table for Dept. Managers
+CREATE TABLE Managers (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create another table for the data in salaries.csv
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE "Department Employees" (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Titles (
+	emp_no VARCHAR NOT NULL,
+	title INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (title) REFERENCES Titles (title),
+	FOREIGN KEY (from_date) REFERENCES from_date (from_date),
+	PRIMARY KEY (emp_no)
+);
+
+
+SELECT * FROM departments;
