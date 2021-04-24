40317885dc7d78cf60bbbe06c0fc9dcd88085e93
Scott Hammel <scott_hammel@icloud.com>
2021-04-24 13:42:12 -0500

    updated CREATE TABLE script.

--- queries.sql
+++ queries.sql
@@ -41,18 +41,18 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp (	
 emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no,dept_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 
 CREATE TABLE titles (
-emp_no VARCHAR (5) NOT NULL,
-	title INT NOT NULL,
+emp_no INT NOT NULL,
+	title VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -73,7 +73,8 @@ SELECT * FROM salaries;
 
 SELECT * FROM dept_emp;
 
-DROP TABLE dept_emp CASCADE;
+-- Drop table to edit
+DROP TABLE "" CASCADE;
 
 -- Determine Retirement Eligibility
 SELECT first_name, last_name
